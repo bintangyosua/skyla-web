c3a70e7b5e838aaef7053d2380929b6ac9076fc8
Bintang <animerevindo@gmail.com>
2023-07-12 14:15:38 +0700

    link, not use a tag

--- src/components/NavbarComponent.tsx/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx/NavbarComponent.tsx
@@ -12,16 +12,18 @@ export default function NavbarComponent() {
     <header className="text-gray-300 body-font">
       <div
         className={`container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center lg:w-[1136px]`}>
-        <a
+        <Link
           className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
           href="/">
           <LogoComponent />
           <span className="ml-3 text-xl">Skyla</span>
-        </a>
+        </Link>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 font-semibold hover:text-white" href="/dashboard">
+          <Link
+            className="mr-5 font-semibold hover:text-white"
+            href="/dashboard">
             Dashboard
-          </a>
+          </Link>
           <a className="mr-5 font-semibold hover:text-white">Invite Bot</a>
           <a className="mr-5 font-semibold hover:text-white">Join Server</a>
         </nav>
