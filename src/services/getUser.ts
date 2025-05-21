f460e866c637a02e663244cd5f8ac2b5cde5e536
Bintang <itzbintangyosua@gmail.com>
2025-05-21 19:31:13 +0700

    fix: fixed deploy

--- src/services/getUser.ts
+++
@@ -1,7 +0,0 @@
-export default async function getServerSideProps(id: string) {
-  const response = await fetch(`/api/discord/users/${id}`);
-  // console.log(id);
-  const data = await response.json();
-  // console.log(data);
-  return { props: { data: data } };
-}
